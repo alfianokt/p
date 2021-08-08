6e3f163f1a3759fcff67ac8c01ecd3f8eaf0eaf0
Alfian Oktafireza <alfianokt104@gmail.com>
2021-08-08 23:21:59 +0700

    feat: add console

---
+++ console/main.js
@@ -0,0 +1,11 @@
+(() => {
+  console.log("%cJasa Pembuatan Aplikasi!", "font-size: 22px; color: red;");
+  console.log(
+    "%cIngin membuat aplikasi website, landing page, toko online dan lainnya?",
+    "font-size: 16px; color: green;"
+  );
+  console.log(
+    "%cSilahkan klik link https://suka.page.link/wa",
+    "font-size: 16px;"
+  );
+})();
